bbde013d4dcda8f2160eb4c4eb529a2bcc3e962b
Евгений <skizee@yandex.ru>
2016-07-07 14:16:14 +0700

    добавил функцию draw_grid(), которая рисует сетку, координатные линии и подписывает цифры

--- drawman.py
+++ drawman.py
@@ -1,24 +1,86 @@
 from turtle import Turtle
+from draw_number import draw_number
 default_scale = 10
+
 
 def init_drawman():
     global t,x_current,y_current, _drawman_scale
     t = Turtle()
+    t.speed(100)
     t.penup()
     x_current = 0
     y_current = 0
     t.goto(x_current,y_current)
     drawman_scale(default_scale)
 
+def drawman_color(color):
+    global t
+    t.color(color)
+
+
 def drawman_scale(scale):
     global _drawman_scale
     _drawman_scale = scale
 
 
+def draw_grid(x_grid_scale=10,y_grid_scale=10):
+    t.color("lightgrey")
+    for x in range(0,500,x_grid_scale * _drawman_scale):
+        t.penup()
+        t.goto(x,-500)
+        t.pendown()
+        t.goto(x,500)
+    for x in range(0,-500,-x_grid_scale * _drawman_scale):
+        t.penup()
+        t.goto(x,-500)
+        t.pendown()
+        t.goto(x,500)
+    for y in range(0,500,y_grid_scale * _drawman_scale):
+        t.penup()
+        t.goto(-500,y)
+        t.pendown()
+        t.goto(500,y)
+    for y in range(0,-500,-y_grid_scale * _drawman_scale):
+        t.penup()
+        t.goto(-500,y)
+        t.pendown()
+        t.goto(500,y)
+    t.color("black")
+    t.penup()
+    t.goto(-500,0)
+    t.pendown()
+    t.goto(500,0)
+    t.penup()
+    t.goto(0,-500)
+    t.pendown()
+    t.goto(0,500)
+
+    t.penup()
+    xx = -x_grid_scale
+    for x in range(0,500,x_grid_scale * _drawman_scale):
+        t.goto(x+2,-15)
+        xx += x_grid_scale
+        draw_number(t,xx)
+    xx = x_grid_scale
+    for x in range(0,-500,-x_grid_scale * _drawman_scale):
+        t.goto(x+2,-15)
+        xx -= x_grid_scale
+        draw_number(t,-xx)
+    yy = -y_grid_scale
+    for y in range(0,500,y_grid_scale * _drawman_scale):
+        t.goto(2,y)
+        yy += y_grid_scale
+        draw_number(t,yy)
+    yy = y_grid_scale
+    for y in range(0,-500,-y_grid_scale * _drawman_scale):
+        t.goto(2,y)
+        yy -= y_grid_scale
+        draw_number(t,-yy)
+
 def test_drawman():
     """
     тестирование работы чертежнжика
-    :return: ybxtuj
+    :return:
     """
     pen_down()
     for i in range(5):
